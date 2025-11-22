c4561065d3a7dc570d8c225aea8b305aea0b50df
Teddy <teddyrinah@gmail.com>
2025-11-22 11:45:30 +0300

    afficher tous les description dans la categorie de formation

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -136,28 +136,11 @@ export default function Catalogue() {
 
                     <CardHeader className="relative z-10 p-2 sm:p-4">
                       <div className="flex items-center gap-1.5 sm:gap-3">
-                        {/* <div
-                          className={`p-1 sm:p-2 rounded-lg ${
-                            cardStyle.split(" ")[2]
-                          }`}>
-                          <svg
-                            className="w-2.5 h-2.5 sm:w-5 sm:h-5 text-white"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24">
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-                            />
-                          </svg>
-                        </div> */}
                         <div className="flex-1 min-w-0">
                           <CardTitle className="text-xs sm:text-lg font-bold text-white truncate">
                             {category.name.toLocaleUpperCase()}
                           </CardTitle>
-                          <CardDescription className="text-[10px] sm:text-sm line-clamp-1 text-white/90">
+                          <CardDescription className="text-[10px] sm:text-sm whitespace-normal text-white/90">
                             {stripHtmlTags(category.description)}
                           </CardDescription>
                         </div>
